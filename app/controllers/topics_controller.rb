8151642f0111d73aac7b0435e76e571f3e6ec30b
TheAliErrachid <errachid.ali.perso@gmail.com>
2023-09-01 11:51:58 +0200

    user story flow ok

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,10 @@
 class TopicsController < ApplicationController
   def index
-    @topics = Topic.all
+    topics = Topic.all.select { |topic| topic.user_topics.count == 1 }
+    topics = Topic.where(id: topics.map { |topic| topic.id }) # .map(&:id)
+    @user_themes_topics = topics.joins(:theme).where(themes: { id: current_user.themes.pluck(:id) })
+    @other_themes_topics = topics.where.not(id: @user_themes_topics.pluck(:id))
+    @joined_topics = current_user.joined_topics
   end
 
   def new
--- app/controllers/user_topics_controller.rb
+++ app/controllers/user_topics_controller.rb
@@ -1,3 +1,7 @@
 class UserTopicsController < ApplicationController
-
+  def create
+    topic = Topic.find(params[:topic_id])
+    UserTopic.create(user: current_user, topic: topic)
+    redirect_to topic_path(topic)
+  end
 end
